78128a40e7bebe0ebd92f4136e3b0a4b270bdc0f
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-03-29 12:42:19 +0200

    deleted serviceworker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 
 createApp(App).use(router).use(store).mount('#app')
-registerSW()
+// registerSW()
