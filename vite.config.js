a002e74bfac9ffa28314db66de8ae0253c94f011
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-03-29 17:11:01 +0200

    fixed project pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/undermapp/' : '/',
   plugins: [
     VitePWA({
       manifest: {
